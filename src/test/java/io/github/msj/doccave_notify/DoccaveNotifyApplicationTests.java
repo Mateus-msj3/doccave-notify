cb8a260cb37fe34e7484df451df695023514ec30
Mateus-msj3 <mateussouzaads@outlook.com>
2025-02-28 19:57:23 -0300

    first commit

---
+++ src/test/java/io/github/msj/doccave_notify/DoccaveNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.msj.doccave_notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoccaveNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
